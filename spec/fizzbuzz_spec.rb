75daf76859ac6b4447e9cfed6162711b5d5977bd
Mace <maceplus@gmail.com>
2020-09-23 23:37:20 +0100

    fizzbuzz describe test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,13 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed number divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
-  end 
-end 
+  end
+  it 'returns "buzz" when passed number divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed number divisible by both' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
